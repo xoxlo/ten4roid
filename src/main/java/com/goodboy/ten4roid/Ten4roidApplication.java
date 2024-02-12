81497c4c6d6a7939553746a5727b44041e5ca73d
ten4roid <thdckdtjr8@gmail.com>
2024-02-12 09:43:02 +0900

    init

---
+++ src/main/java/com/goodboy/ten4roid/Ten4roidApplication.java
@@ -0,0 +1,13 @@
+package com.goodboy.ten4roid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ten4roidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ten4roidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodboy/ten4roid/Ten4roidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodboy.ten4roid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ten4roidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
